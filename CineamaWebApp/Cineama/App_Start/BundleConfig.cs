cd5a418f5305d742402a694e588892c7d824ebeb
Dumitru Muntean <261824@via.dk>
2018-11-09 23:52:04 +0100

    carousel created

--- CineamaWebApp/Cineama/App_Start/BundleConfig.cs
+++ CineamaWebApp/Cineama/App_Start/BundleConfig.cs
@@ -20,11 +20,13 @@ namespace Cineama
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/Carousel.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/Carousel.css"));
         }
     }
 }
--- CineamaWebApp/Cineama/Controllers/HomeController.cs
+++ CineamaWebApp/Cineama/Controllers/HomeController.cs
@@ -1,16 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using Cineama.Models;
+using System.Data.Entity.Infrastructure;
+using System.Threading.Tasks;
+
 
 namespace Cineama.Controllers
 {
     public class HomeController : Controller
     {
+        private ApplicationDbContext db = new ApplicationDbContext();
         public ActionResult Index()
         {
-            return View();
+            return View(db.MovieModels.ToList());
         }
 
         public ActionResult About()
